d2dcbb077b3e7636b558f1e8e872b4e837927823
Thom Chiovoloni <chiovolonit@gmail.com>
2021-06-02 09:09:25 -0700

    asdf

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ fn main() {
     println!("cargo:rerun-if-env-changed=REBUILD");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src");
+    println!("cargo:warning=cargo is sometimes okay if you know about this");
     let read = false;
     let mut ttyout = std::fs::OpenOptions::new()
         .write(true)
@@ -11,7 +12,7 @@ fn main() {
         .expect("failed to open /dev/tty");
     write!(
         ttyout,
-        "\n\x1b[1;33mcargo doesnt want you to know this, but it's a warning\x1b[m\n"
+        "\n\x1b[1;33mwarning\x1b[39m:\x1b[0m cargo doesn't want you to know about this\n",
     )
     .expect("failed to write");
     ttyout.flush().expect("failed to flush");
