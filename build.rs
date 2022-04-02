ed3994983ed519fa8432e1a84a22e67a5cb6dc89
Thom Chiovoloni <chiovolonit@gmail.com>
2022-04-02 12:24:45 -0700

    windows support?

--- build.rs
+++ build.rs
@@ -5,11 +5,12 @@ fn main() {
     println!("cargo:rerun-if-changed=src");
     println!("cargo:warning=cargo is sometimes okay if you know about this");
     let read = false;
+    let path = if cfg!(windows) { "CONOUT$" } else { "/dev/tty" };
     let mut ttyout = std::fs::OpenOptions::new()
         .write(true)
         .read(read)
-        .open("/dev/tty")
-        .expect("failed to open /dev/tty");
+        .open(path)
+        .unwrap_or_else(|e| panic!("failed to open {:?}: {:?}", path, e));
     write!(
         ttyout,
         "\n\x1b[1;33mwarning\x1b[39m:\x1b[0m cargo doesn't want you to know about this\n",
